58dd0400ff50d37b62fb52a801d18496f8b542a8
mmendoza83 <mmendoza83@student.cccd.edu>
2017-09-21 15:48:49 -0700

    End of class commit.

---
+++ app/src/main/java/edu/orangecoastcollege/cs273/mmendoza/ocmusicevents/EventDetailsActivity.java
@@ -0,0 +1,61 @@
+package edu.orangecoastcollege.cs273.mmendoza.ocmusicevents;
+
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class EventDetailsActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_details);
+
+        // Get the data out of the Intent
+        Intent intent = getIntent();
+        String title = intent.getStringExtra("title");
+        String details = intent.getStringExtra("details");
+
+        // Create references to the text views
+        TextView titleTextView = (TextView) findViewById(R.id.eventTitleTextView);
+        TextView detailsTextView = (TextView) findViewById(R.id.eventDetailsTextView);
+        ImageView eventImageView = (ImageView) findViewById(R.id.eventImageView);
+
+        // Set the text of the text views
+        titleTextView.setText(title);
+        detailsTextView.setText(details);
+
+        // Use the Asset Manager to retrieve a file (image)
+        AssetManager am = getAssets();
+        String imageFileName = title.replace(" ", "") + ".jpeg";
+
+        // Use the AssetManager to open a string to the file name
+        try
+        {
+            InputStream stream = am.open(imageFileName);
+            Drawable image = Drawable.createFromStream(stream, title);
+            eventImageView.setImageDrawable(image);
+        }
+        catch (IOException e)
+        {
+            Log.e("OC Music Events", "Error loading image: " + imageFileName);
+        }
+    }
+
+    protected void goBackToList(View v)
+    {
+        // Terminates the current activity
+        finish();
+    }
+}
